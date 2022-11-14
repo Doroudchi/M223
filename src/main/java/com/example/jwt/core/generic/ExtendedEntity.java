5d2080f308c425bb29507342426da8df7b213db9
Tehran Doroudchi <ali.doroudchi@interdisount.ch>
2022-11-14 11:34:47 +0100

    Initial commit with Spring security

---
+++ src/main/java/com/example/jwt/core/generic/ExtendedEntity.java
@@ -0,0 +1,40 @@
+package com.example.jwt.core.generic;
+
+import com.example.jwt.domain.user.User;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
+public abstract class ExtendedEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    protected ExtendedEntity() {
+    }
+
+    protected ExtendedEntity(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public ExtendedEntity setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+}
